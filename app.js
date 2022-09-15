f999b2c0831f4f23741f59571d83ab43d995ca56
Ashley Tucker <ashleyst35@aol.com>
2022-09-15 10:14:12 -0400

    exercise

--- app.js
+++ app.js
@@ -3,6 +3,45 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
+const firstName = "Mayci"; // not reassigned bc its a constant variable
+let lastName ="Asa";
+var age = 51;
+
+//firstName = "Frank"; // reassignment not work bc its a constant
+lastName = "Donnie" // can reassign last name bc its a let
+age = 25  // age as well can reassign bc let is same as var
+
+let language = "JavaScript"
+let createdYear = 1995; // could have used var instead of let, if use const the value cant change
+let isCaseSensitive = true;
+
+console.Log ('${language} was first released in ${createdYear}.');
+
+let product =  "Trucker hat"
+let price = 19.99; // double bc of decimal point
+let isOnSale = false;
+let salePercentage = 15; 
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+console.Log(`$ The ${product} in size ${selectedSize} is $${price} has a \n
+${salePercentage} percent off tag, with ${stock} in stock.`
+);
+
+let title ="Name of the Wind";
+let author ="Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+console.Log(
+  `$ Have you read ${title}, by ${author}? It is ${pageCount} pages of perfection,\n
+   I have page ${bookmark} marked with my favorite ${hasRead} life quote! `
+);
+
+
+
 // Exercise 1
 
 // Exercise 2
